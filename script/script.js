040c13f5f5597d0453cba994f2f40a6109576e80
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-02-28 15:31:55 +0100

    Add status bar (not fully functioning) and reformat functions

--- script/script.js
+++ script/script.js
@@ -1,56 +1,66 @@
 //VARIABLES
-const actionButtons = document.querySelectorAll('.button')
-const timeButtons = document.querySelectorAll('.knob')
-const timerDisplay = document.querySelector('.timer')
-const sessDisplay = document.querySelector('.sess-time')
-const breakDisplay = document.querySelector('.break-time')
-const workLight = document.querySelector('.work')
-const breakLight = document.querySelector('.break')
+const playButton = document.querySelector('#play');
+const pauseButton = document.querySelector('#pause');
+const stopButton = document.querySelector('#stop');
+const timeButtons = document.querySelectorAll('.knob');
+const timerDisplay = document.querySelector('.timer');
+const sessDisplay = document.querySelector('.sess-time');
+const breakDisplay = document.querySelector('.break-time');
+const workLight = document.querySelector('.work');
+const breakLight = document.querySelector('.break');
+const dots = document.querySelectorAll('.circle');
 
 let timeSetting = 25;
 let breakSetting = 5;
 let min = timeSetting;
 let sec = 0;
-let play = false;
+let go = false;
+let pause = false;
+let tick;
+let counter = 0;
+let worked = true;
+let theDots = [];
 
 //FUNCTIONS
-function countdown(e) {
-    action = e.target.id;
-    let tick = setInterval(function() {
-        switch (action) {
-            case "pause":
-                clearInterval(tick);
-                break;
-            case "stop":
-                clearInterval(tick);
-                reset();
-                break;
-            case "play":
-                if (play == false) {play = true};    
-                break;
-            default:
-                break;
-        };
-
-        if (play) {
-                if (sec > 0) {
-                    sec -= 1;
-                } else {
-                    min -= 1;
-                    sec = 59;
-                };
-                timerDisplay.textContent = `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
-                
-                if (min == 0 && sec == 0) {
-                    clearInterval(tick);
-                    reset();
-                };
+function run() {
+    if (!go) {
+        go = true;
+        theDots[0].classList.add('dot');
+        tick = setInterval(function() {
+            if (sec > 0) {
+                sec -= 1;
+            } else {
+                min -= 1;
+                sec = 59;
             };
-    }, 1000);
+            timerDisplay.textContent = `${min}:${sec.toString().padStart(2, '0')}`;
+            transition();
+        }, 1000); 
+    }   
 };
 
-function reset() {
-    play = false;
+function transition() {
+    if (min == 0 && sec == 0) {
+        counter += 1;
+        clearInterval(tick);
+        if (counter <= 8) {
+            if (worked == true) {
+                worked = false;
+                smallReset(breakSetting); 
+            } else {
+                worked = true;
+                smallReset(timeSetting)  
+            };
+        } else {
+            timerDisplay.textContent = "COMPLETE!";
+        };
+    };
+};
+
+function hardReset() {
+    clearInterval(tick);
+    pause = false;
+    go = false;
     timeSetting = 25;
     breakSetting = 5;
     min = timeSetting;
@@ -58,20 +68,45 @@ function reset() {
     timerDisplay.textContent = "25:00";
     sessDisplay.textContent = "25:00";
     breakDisplay.textContent = "5:00";
+    workLight.classList.remove('light');
+    breakLight.classList.remove('light');
+    counter = 0;
+    theDots.forEach((dot) => (dot.classList.remove('dot')));
+};
+
+function smallReset(minutes) {
+    pause = false;
+    go = false;
+    sec = 0;
+    min = minutes;
+    breakLight.classList.toggle('light');
+    workLight.classList.toggle('light');
+    timerDisplay.textContent = `${min}:${sec.toString().padStart(2, '0')}`;
+    run();
+};
+
+function rest() {
+    if (go == true) {
+        go = false;
+        pause = true;
+    };
+    clearInterval(tick);
 };
 
 function increment(e) {
     level = e.target.id;
-    if (!play) {
+    if (!go && !pause) {
         switch (level) {
             case "sess-down":
                 timeSetting -= 1;
                 sessDisplay.textContent = `${timeSetting}:00`;
+                timerDisplay.textContent = `${timeSetting}:00`;
                 min = timeSetting;
                 break;
             case "sess-up":
                 timeSetting += 1;
                 sessDisplay.textContent = `${timeSetting}:00`;
+                timerDisplay.textContent = `${timeSetting}:00`;
                 min = timeSetting;
                 break;
             case "break-down":
@@ -87,9 +122,11 @@ function increment(e) {
 };
 
 //TO RUN
-reset();
-actionButtons.forEach((button) => {button.addEventListener('click', countdown)});
-
+hardReset();
+workLight.classList.toggle('light');
+pauseButton.addEventListener('click', rest);
+playButton.addEventListener('click', run);
+stopButton.addEventListener('click', hardReset);
 timeButtons.forEach((button) => {button.addEventListener('click', increment)});
-
+dots.forEach((dot) => {theDots.push(dot)});
 
